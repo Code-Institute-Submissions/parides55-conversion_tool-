bc4c512a12436b90433d201573f288bf28f1f066
parides55 <andreasparides5@gmail.com>
2024-01-18 10:24:42 +0000

    validate the input of user in the start_unit_conversion function by calling the validate_input function

--- run.py
+++ run.py
@@ -1,6 +1,34 @@
 import requests
 import json
 
+CONVERSINOS = [
+        ('(1)', 'Currency Conversion'),
+        ('(2)', 'Unit Conversion'),
+    ]
+AVAILABLE_CONVERSIONS = [
+    ('km','metres'),
+    ('km', 'miles'),
+    ('km', 'feet'),
+    ('km', 'Nm'),
+    ('km', 'Yards'),
+    ('metres', 'inches'),
+    ('sqKm', 'sqmetres'),
+    ('sqKm', 'sqmiles'),
+    ('sqKm', 'sqfeet'),
+    ('sqKm', 'sqNM'),
+    ('sqKm', 'Acre'),
+    ('kg', 'gramms'),
+    ('kg', 'pounds'),
+    ('kg', 'stones'),
+    ('kg', 'miligramms'),
+    ('km/h', 'metres/h'),
+    ('km/h', 'miles/h'),
+    ('km/h', 'knots'),
+    ('litres', 'mililtres'),
+    ('litres', 'gallons(us)'),
+    ('litres', 'gallons(uk)'),
+]
+
 def make_selection():
     """
     This function takes user's selections, 
@@ -18,17 +46,11 @@ def make_selection():
             calculate_currency()
             break
         elif conversion_selection == '2':
-            print()
-            print('UNIT CONVERSION\n')
-            unit_from = input("Enter a unit you would like to convert from :")
-            unit_to = input("Enter a unit you would like to convert to :")
-            value = int(input(f"Enter the amount of {unit_from} to convert to {unit_to} :"))
-            to_calculate = Calculator(unit_from, unit_to, value)
-            to_calculate.make_conversion()
+            start_unit_conversions()
             break
         
-    return conversion_selection
-    
+    return None
+
 def validate_conversion_selection(selection):
     """
     Validates the user's input.
@@ -43,7 +65,16 @@ def validate_conversion_selection(selection):
         return False
     
     return True
-
+    
+def validate_input(str1, str2):
+    for x, y in AVAILABLE_CONVERSIONS:
+            if str1 != x and str2 != y:
+                print(f'Please type a unit from the list above.\nFor example, for kilometres type km. You have typed "{str1}" and "{str2}"'
+                    )
+                return False
+            else:
+                return True    
+    
 def repeat():
     """
     A function which is called after every conversion is made and gives the option to
@@ -66,10 +97,11 @@ def calculate_currency():
     print()
     print('CURRENCY CONVERSION\n')
     
+    # fetches a dictionary of all currencies used in the frankfurter api and returns a list
     url = "https://api.frankfurter.app/currencies"
     response_currencies = requests.request("GET", url)
     dict_currency = json.loads(response_currencies.text)
-    list_currency = list(dict_currency.keys())
+    list_currency = list(dict_currency.items())
     print('Available Currency Codes to use\n')
     print(list_currency)
     print()
@@ -83,6 +115,29 @@ def calculate_currency():
     print(f"{amount} {from_rate} is {amount_converted.json()['rates'][to_rate]} {to_rate}")
     
     repeat()
+
+def start_unit_conversions():
+    """
+    When it's called shows the availabel unit conversions and
+    asks the user to make inputs. These inputs then are passed to the 
+    Calculator class.
+    """
+    print()
+    print('UNIT CONVERSION\n')
+    print('Available Corvesion')
+    print(AVAILABLE_CONVERSIONS)
+    print()
+    while True:
+        unit_from = input("Enter a unit you would like to convert from :")
+        unit_to = input("Enter a unit you would like to convert to :")
+        #check(unit_from, unit_to)
+        if validate_input(unit_from, unit_to):
+            value = int(input(f"Enter the amount of {unit_from} to convert to {unit_to} :"))
+            to_calculate = Calculator(unit_from, unit_to, value)
+            to_calculate.make_conversion()
+            break
+    return None        
+    
  
 class Calculator:
     """
@@ -100,15 +155,15 @@ class Calculator:
         """
         Takes the units and the value entered from the user to make the conversion.
         """    
-        if self.unit_from == 'kilometres' and self.unit_to == 'metres':
+        if self.unit_from == 'km' and self.unit_to == 'metres':
             converted_value = self.value * 1000
-        elif self.unit_from == 'kilometres' and self.unit_to == 'miles':
-            converted_value = self.value * 1.60934
-        elif self.unit_from == 'metres' and self.unit_to == 'kilometres':
+        elif self.unit_from == 'km' and self.unit_to == 'miles':
+            converted_value = self.value * 0.60934
+        elif self.unit_from == 'metres' and self.unit_to == 'km':
             converted_value = self.value / 1000
         elif self.unit_from == 'metres' and self.unit_to == 'miles':
             converted_value = self.value * 0.000621371
-        elif self.unit_from == 'miles' and self.unit_to == 'kilometres':
+        elif self.unit_from == 'miles' and self.unit_to == 'km':
             converted_value = self.value * 1.60934
         elif self.unit_from == 'miles' and self.unit_to == 'metres':
             converted_value = self.value * 1609.34
@@ -121,12 +176,7 @@ class Calculator:
 def start_up():
     """
     Run all programms
-    """
-    CONVERSINOS = [
-        ('(1)', 'Currency Conversion'),
-        ('(2)', 'Unit Conversion'),
-    ]
-    
+    """    
     print("What would you like to convert?")
     print()
 
